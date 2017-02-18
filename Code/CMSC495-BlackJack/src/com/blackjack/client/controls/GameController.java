91e2c2767a61e01b471475682b0570c5facd0bba
Palmy523 <Palmy523@msn.com>
2017-02-18 05:55:31 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Code/CMSC495-BlackJack/src/com/blackjack/client/controls/DealerAI.java

--- Code/CMSC495-BlackJack/src/com/blackjack/client/controls/GameController.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/controls/GameController.java
@@ -17,15 +17,16 @@ public class GameController {
 	private Dashboard dashboard;
 	private BlackJackGamePanel gamePanel;
 	private GameState gameState;
-	
+
 	public GameController(Dashboard dashboard, GameState gameState) {
 		this.dashboard = dashboard;
 		gamePanel = dashboard.getGamePanel();
 		this.gameState = gameState;
 	}
-	
+
 	/**
 	 * Starts a game of 21!
+	 * 
 	 * @param room
 	 * @param isEasyPlay
 	 */
@@ -34,166 +35,68 @@ public class GameController {
 		gamePanel.disableAllButtons();
 		GameState.setTurn(TurnState.AWAITING_BET);
 	}
-	
+
 	public void quitGame() {
-		//TODO quit the game and return to the RoomSelectionScreen
+		// TODO quit the game and return to the RoomSelectionScreen
 	}
-	
-	public void deal() {		
-		if(canDeal()){
-			//Set the GameState.TurnState to PLAYER_TURN
-			GameState.setTurn(TurnState.DEALER_TURN);
-			//Create a GameEvent with the current GameState.
-			GameEvent event = new GameEvent(gameState);		
-			//Create a new DealAction
-			DealAction action = new DealAction(100, gamePanel);
-			//Invoke the processAction method of the DealAction
-			action.processAction(event);
-		}		
+
+	public void deal() {
+		// Create a GameEvent with the current GameState.
+		GameEvent event = new GameEvent(gameState);
+		event.setActionType(ActionType.DEAL);
+		// Create a new DealAction
+		DealAction action = new DealAction(100, gamePanel);
+		// Invoke the processAction method of the DealAction
+		action.processAction(event);
 	}
-	
-	public boolean canDeal() {
-		//Determine if game is in a state that allows a deal
-		if(GameState.getTurn() == TurnState.AWAITING_DEAL)
-			return true;
-		return false;
-	}
-	
-	public void hit(HandType type) {
-		if (type == HandType.PLAYER) {
-			playerHit();
-		} else {
-			dealerHit();
-		}
-	}
-	
+
 	public void playerHit() {
-		if (canPlayerHit()) {
-			GameEvent event = new GameEvent(gameState);
-			HitAction action = new HitAction(gamePanel);
-			action.processAction(event);
-		}
+		GameEvent event = new GameEvent(gameState);
+		event.setActionType(ActionType.HIT);
+		HitAction action = new HitAction(gamePanel);
+		action.processAction(event);
 	}
-	
-	public boolean canPlayerHit() {
-		//TODO determine if the game is in a state that allows the player to hit
-		if(GameState.getTurn() == TurnState.PLAYER_TURN){
-			return true;
-		}
-		return false;
-	}
-	
+
 	public void dealerHit() {
-		if (canDealerHit()) {
-			GameEvent event = new GameEvent(gameState);
-			HitAction action = new HitAction(gamePanel);
-			action.processAction(event);
-		}
+		GameEvent event = new GameEvent(gameState);
+		event.setActionType(ActionType.HIT);
+		HitAction action = new HitAction(gamePanel);
+		action.processAction(event);
 	}
-	
-	public boolean canDealerHit() {
-		if(GameState.getTurn() == TurnState.DEALER_TURN){
-			return true;
-		}
-		return false;
-	}
-	
-	/*public void stand(Hand hand) {
-		//TODO perform a stand action for the specified hand
-	}*/
-	public void stand(HandType type) {
-		if(type == HandType.PLAYER){
-			playerStand();
-		}else{
-			dealerStand();
-		}
-	}
-	
+
 	public void playerStand() {
-		//TODO cause a player hand to stand
-		if(canPlayerStand()){
-			GameEvent event= new GameEvent(gameState);
-			StandAction action = new StandAction(gamePanel);
-			action.processAction(event);			
-		}
+		GameEvent event = new GameEvent(gameState);
+		event.setActionType(ActionType.STAND);
+		StandAction action = new StandAction(gamePanel);
+		action.processAction(event);
 	}
-	
-	public boolean canPlayerStand() {
-		//TODO determine if the game is in a state that allows the player to Stand.
-		if(gameState.getTurn() == TurnState.PLAYER_TURN){
-			return true;
-		}
-		return false;
-	}
-	
-	public void dealerStand() {
-		if(canDealerStand()){
-			GameEvent event= new GameEvent(gameState);
-			StandAction action = new StandAction(gamePanel);
-			action.processAction(event);			
-		}
-		//TODO cause the dealer hand to stand
-	}
-	
-	public boolean canDealerStand() {
-		return GameState.getTurn() == TurnState.DEALER_TURN;
-	}
-	
+
 	public void insurance() {
-		//TODO invokes insurance for the player
+		// TODO invokes insurance for the player
 	}
-	
-	public boolean canInsurance() {
-		//TODO determine if the game is in a state that allows for insurance
-		return false;
-	}
-	
+
 	public void doubleDown() {
-		//TODO cause the player to Double Down
+		// TODO cause the player to Double Down
 	}
-	
-	public boolean canDoubleDown() {
-		//TODO determine if the game is in a state that allows the player to double down
-		return false;
-	}
-	
+
 	public void split() {
-		//TODO splits the players hand
-		
+		// TODO splits the players hand
+
 	}
-	
-	public boolean canSplit() {
-		//TODO determine if the game is in a state that allows a split
-		return false;
-	}
-	
+
 	public void surrender() {
-		//TODO causes the player to surrender
+		// TODO causes the player to surrender
 	}
-	
-	public boolean canSurrender() {
-		//TODO determine if the game is in a state that allows a surrender
-		if(GameState.getTurn() == TurnState.PLAYER_TURN){
-			return true;
-		}
-		return false;
-	}
-	
+
 	public void betPlus(int amount) {
-		if (canBet()) {
-			GameEvent event = new GameEvent(gameState);
-			event.setActionType(ActionType.BET);
-			BetAction action = new BetAction(gamePanel, amount);
-			action.processAction(event);
-		}
+		GameEvent event = new GameEvent(gameState);
+		event.setActionType(ActionType.BET);
+		BetAction action = new BetAction(gamePanel, amount);
+		action.processAction(event);
 	}
-	
+
 	public void betMinus(int amount) {
-		//TODO decrease the bet by the specified amount
-	}
-	
-	public boolean canBet() {
-		return GameState.getTurn() == TurnState.AWAITING_BET;
+		// TODO decrease the bet by the specified amount
 	}
 
 	public GameState getGameState() {
@@ -203,15 +106,5 @@ public class GameController {
 	public void setGameState(GameState gameState) {
 		this.gameState = gameState;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
