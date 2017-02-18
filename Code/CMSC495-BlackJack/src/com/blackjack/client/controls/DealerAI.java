a7dbe2a1745b405b27ac86baede545f2db0a41c8
lea <lere.8811@gmail.com>
2017-02-18 00:06:12 -0500

    pseudocode 2

--- Code/CMSC495-BlackJack/src/com/blackjack/client/controls/DealerAI.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/controls/DealerAI.java
@@ -1,85 +1,41 @@
 package com.blackjack.client.controls;
 
-import com.blackjack.client.action.GameAction;
 import com.blackjack.client.entities.GameState;
 import com.blackjack.client.entities.GameState.TurnState;
+import com.blackjack.client.event.GameEvent;
+import com.blackjack.client.ui.BlackJackGamePanel;
+import com.google.gwt.user.client.Timer;
 
-/**
- * 
- * @author Lea
- *
- */
 public class DealerAI {
 
 	private GameController controller;
-	private double proc = Math.random();
+	private BlackJackGamePanel gamePanel;
+	private Timer timer;
 
-	public DealerAI(GameController controller) {
+	public DealerAI(GameController controller, BlackJackGamePanel panel) {
 		this.controller = controller;
+		this.gamePanel = panel;
 	}
 
-	@SuppressWarnings("static-access")
-	public void startTurn() {
-
-		//TODO proc off a series of actions on at a time
-		while (controller.getGameState().getTurn() == TurnState.DEALER_TURN) {
-			//TODO do some stuff with the dealer by creating GameActions (HIT, STAND) 
-			//until the dealer chooses to stand or busts, when that happens, update the GameState
-			//to PlayerTurn
-			
-			// start timer that processes a method that is one turn for the dealer
-			
-			
-			// on each proc of the timer the method will be processed until the Dealer decides to
-			// Stand or Busts
-			
-			
-			
-			
-			// A Stand or Bust Event listener needs to be created, so the timer can be stopped
-			// and HandEndAction can be called when the dealer busts or stands.
-			
-			controller.dealerHit();
-			
-//			if (lowstakes) {
-//
-//				//70%
-//				if (controller.canDealerHit() && ()proc < 0.7) {
-////					GameAction.ActionType.HIT;
-////					GameAction.ActionType.STAND;
-//					controller.dealerHit();
-//					controller.canDealerStand();
-//				}
-//
-//			} else if (medium_stakes) {
-//
-//				//50%
-//				if (proc < 0.5) {
-////					GameAction.ActionType.HIT;
-////					GameAction.ActionType.STAND;
-//					controller.dealerHit();
-//					controller.canDealerStand();
-//				}
-//
-//			} else if (high_stakes) {
-//
-//				//20%
-//				if (proc < 0.2) {
-////					GameAction.ActionType.HIT;
-////					GameAction.ActionType.STAND;
-//					controller.dealerHit();
-//					controller.canDealerStand();
-//				}
-//
-//				
-//			}
-			
-			//when the dealer chooses to stand or bust HandEndAction 
-			//processAction is called
-			controller.getGameState().setTurn(TurnState.HAND_END);
-		}
+	public void startTurn(final GameEvent event) {
 		
-		
+		timer = new Timer() {
 
+			@Override
+			public void run() {
+				while (GameState.getTurn() == TurnState.DEALER_TURN) {
+					processTurn(event);
+				}
+			}
+		};
+		
+		timer.scheduleRepeating(1000);
 	}
+
+	public void processTurn(GameEvent event) {
+		// TODO set to actual hand value from Jeffs update
+		int playerHandValue = 0;
+		
+	}
+
 }
