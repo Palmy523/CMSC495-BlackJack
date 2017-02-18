a5e2a27a14434b3a0aeca6d9ea8515a8b1219ac4
lea <lere.8811@gmail.com>
2017-02-18 03:12:10 -0500

    added HandEndAction notes and some initial source code

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/HandEndAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/HandEndAction.java
@@ -7,6 +7,12 @@ import com.blackjack.client.event.Events;
 import com.blackjack.client.event.GameEvent;
 import com.blackjack.client.ui.BlackJackGamePanel;
 
+/**
+ * -Check the hand values to determine who wins and determine push
+ * -Award money and update chip amounts appropriately. Calls the 
+ * GameController startGame() method to start a new hand.
+ *
+ */
 public class HandEndAction extends GameAction {
 
 	public HandEndAction(BlackJackGamePanel panel) {
@@ -15,6 +21,29 @@ public class HandEndAction extends GameAction {
 
 	@Override
 	public void processAction(GameEvent event) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub	
+		
+		GameState state = event.getGameState();
+		
+		int playerHandVal = state.getPlayerHand().getHandValue();
+		int dealerHandVal = state.getDealerHand().getHandValue();
+		
+		if (dealerHandVal > playerHandVal) {
+			//TODO UI displays dealer wins
+			//play chip sounds when player loses
+				// balance should already be updated when the betting started, no update
+				// to balance
+		} if (dealerHandVal < playerHandVal) {
+			//TODO UI displays player wins
+			
+			// play chip winning sounds
+			// update players balance 1:1 for normal wins
+			// ...
+		} else {
+			//TODO UI displays a push is made
+			
+		}
+		
+		
 	}
 }
