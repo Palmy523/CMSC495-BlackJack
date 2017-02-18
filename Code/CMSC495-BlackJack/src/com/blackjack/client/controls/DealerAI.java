adf164c4516dc39caa617400654f4b266dd5b823
lea <lere.8811@gmail.com>
2017-02-18 02:46:55 -0500

    edited processTurn() to focus only on the dealer, the dealer will always hit until = 16 regardless of push or win, etc

--- Code/CMSC495-BlackJack/src/com/blackjack/client/controls/DealerAI.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/controls/DealerAI.java
@@ -1,6 +1,6 @@
 package com.blackjack.client.controls;
 
-import com.blackjack.client.action.GameAction;
+import com.blackjack.client.action.BustAction;
 import com.blackjack.client.action.HandEndAction;
 import com.blackjack.client.action.HitAction;
 import com.blackjack.client.action.StandAction;
@@ -22,9 +22,9 @@ public class DealerAI {
 		this.controller = controller;
 		this.gamePanel = panel;
 	}
-	
+
 	public void startTurn(final GameEvent event) {
-		
+
 		timer = new Timer() {
 
 			@Override
@@ -36,54 +36,66 @@ public class DealerAI {
 		};
 		timer.scheduleRepeating(1000);
 	}
-	
+
 	@SuppressWarnings("static-access")
 	public void processTurn(GameEvent event) {
-		
+
 		GameState state = event.getGameState();
-		//TODO set to actual hand value from Jeffs update
-		int playerHandValue = state.getPlayerHand().getHandValue();
-		//TODO set to actual hand value from Jeffs update
+		// //TODO set to actual hand value from Jeffs update
+		// int playerHandValue = state.getPlayerHand().getHandValue();
+		// TODO set to actual hand value from Jeffs update
 		int dealerHandValue = state.getDealerHand().getHandValue();
-		
+
 		boolean hit = shouldHit(dealerHandValue);
-		
+
+		// dealer hit or stand actions
 		if (hit) {
 			HitAction action = new HitAction(gamePanel);
 			action.processAction(event);
+			event.setActionType(ActionType.HIT);
 		} else {
+			timer.cancel();
 			StandAction action = new StandAction(gamePanel);
 			action.processAction(event);
+			event.setActionType(ActionType.STAND);
+
+			// dealerHandValue should be > 0
+			// if dealer did not bust
+			if (dealerHandValue < 22) {
+				HandEndAction endHand = new HandEndAction(gamePanel);
+				endHand.processAction(event);
+			}
 		}
-		
-		if (dealerHandValue == 21) {
+
+		// dealer busts after hit
+		if (dealerHandValue > 21) {
 			timer.cancel();
-			HandEndAction action = new HandEndAction(gamePanel);
+			// TODO UI indicate dealer busted
+			state.setTurn(TurnState.HAND_END);
+
+			BustAction action = new BustAction(100, gamePanel);
 			action.processAction(event);
-		}
-		
-		if (dealerHandValue == 17 && playerHandValue <= 17) {
-			timer.cancel();
-			//TODO UI indicate player's hand is pushed
-			
-			
-			
-			//Fire the event so the rest of the UI knows that the action occurred
+
+			// Fire the event so the rest of the UI knows that the action
+			// occurred
 			event.setActionType(ActionType.BUST);
-			Events.eventBus.fireEvent(event);
 		}
-		
+
+		// Fire the event so the rest of the UI knows that the action occurred
+		Events.eventBus.fireEvent(event);
 	}
-	
+
 	/**
-	 * HIT until dealer's hand value = 17 regardless of a possible push, win,
-	 * or loss
+	 * HIT until dealer's hand value = 17 regardless of a possible push, win, or
+	 * loss
+	 * 
 	 * @param dealerHandValue
 	 * @return boolean hit or no hit
 	 */
 	private boolean shouldHit(int dealerHandValue) {
 		if (dealerHandValue < 17) {
-			//TODO under review, adding medium stakes dealer always hits on a soft 17
+			// TODO under review, adding medium stakes dealer always hits on a
+			// soft 17
 			return true;
 		}
 		return false;
