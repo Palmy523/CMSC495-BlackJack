b5527a02bd1cec898bc1f569901dba746d0498d0
Palmy523 <Palmy523@msn.com>
2017-02-18 19:34:25 -0500

    Updated BetAction With Steph's Changes

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/BetAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/BetAction.java
@@ -1,45 +1,44 @@
 package com.blackjack.client.action;
 
-import java.util.concurrent.Delayed;
-import java.util.concurrent.TimeUnit;
-
 import com.blackjack.client.entities.GameState;
+import com.blackjack.client.entities.GameState.TurnState;
 import com.blackjack.client.event.Events;
 import com.blackjack.client.event.GameEvent;
+import com.blackjack.client.sounds.SoundManager;
+import com.blackjack.client.sounds.SoundManager.SoundName;
 import com.blackjack.client.ui.BlackJackGamePanel;
+import com.blackjack.client.ui.GameButton.GameButtonType;
 
 public class BetAction extends GameAction {
+  private int betAmount;
+  public BetAction(BlackJackGamePanel panel, int betAmount) {
+      super(panel);
+      this.betAmount = betAmount;
+}
 
-	private int betAmount;
-	
-	public BetAction(BlackJackGamePanel panel, int betAmount) {
-		super(panel);
-		this.betAmount = betAmount;
-	}
+@Override
+public void processAction(GameEvent event) {
+      GameState state = event.getGameState();
+  if (GameState.getTurn() != TurnState.AWAITING_BET || 
+		  GameState.getTurn() != TurnState.AWAITING_DEAL)
+      return;
+int currentBet = GameState.getBetAmount() +betAmount;
+  GameState.setBetAmount(currentBet);
+  panel.bet(currentBet);
 
-	@Override
-	public void processAction(GameEvent event) {
-		GameState state = event.getGameState();
-		//Update panel based on state and bet amount, see accessors below 
-		//for potentially required state objects
-		//state.getBetAmount()
-		//state.getDealerHand()
-		//state.getPlayerHand()
-		//state.getDeck()
-		//state.getTurn()
-		
-		//TODO Cause a bet increase on the GameState by the specified amount in the constructor
-		//NOTE: NEGATIVE integer values will be passed in for betMinus, and Positive for betPlus
-		
-		//TODO Play sounds using the SoundManager.play(SoundName) static method!!!!! See SoundManager to create
-		//the sounds you need. Just follow the same setup that FAN1 uses, add an enum name
-		//then add a new sound that follows the creation in the loadResources method that matches 
-		//FAN1 but reference the sound from the war/sounds directory that you want.
-		
-		//TODO update the GameState by setting the proper turn, or other data
-		
-		//Fire the event so the rest of the UI knows that the action occurred
-		event.setActionType(ActionType.BET);
-		Events.eventBus.fireEvent(event);
-	}
+SoundManager.play(SoundName.CHIP_BET);
+
+if (currentBet>= state.getRoom().getMinBet()){
+  panel.enableButton(GameButtonType.DEAL, true);
+  }
+
+if (currentBet> state.getRoom().getMaxBet()){
+    currentBet=state.getRoom().getMaxBet();
+    panel.chipsEnabled(false);
+}
+
+//Fire the event so the rest of the UI knows that the action occurred
+event.setActionType(ActionType.BET);
+Events.eventBus.fireEvent(event);
+  }
 }
