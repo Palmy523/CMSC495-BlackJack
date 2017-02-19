abb8a07f0e399d9a4e2bb0d6dcc2cf31f415c5b4
Palmy523 <Palmy523@msn.com>
2017-02-18 20:11:36 -0500

    BetAction Update
    Slight update to BetAction

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/BetAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/BetAction.java
@@ -8,37 +8,43 @@ import com.blackjack.client.sounds.SoundManager;
 import com.blackjack.client.sounds.SoundManager.SoundName;
 import com.blackjack.client.ui.BlackJackGamePanel;
 import com.blackjack.client.ui.GameButton.GameButtonType;
+import com.google.gwt.core.client.GWT;
 
 public class BetAction extends GameAction {
-  private int betAmount;
-  public BetAction(BlackJackGamePanel panel, int betAmount) {
-      super(panel);
-      this.betAmount = betAmount;
-}
+	private int betAmount;
 
-@Override
-public void processAction(GameEvent event) {
-      GameState state = event.getGameState();
-  if (GameState.getTurn() != TurnState.AWAITING_BET || 
-		  GameState.getTurn() != TurnState.AWAITING_DEAL)
-      return;
-int currentBet = GameState.getBetAmount() +betAmount;
-  GameState.setBetAmount(currentBet);
-  panel.bet(currentBet);
+	public BetAction(BlackJackGamePanel panel, int betAmount) {
+		super(panel);
+		this.betAmount = betAmount;
+	}
 
-SoundManager.play(SoundName.CHIP_BET);
+	@Override
+	public void processAction(GameEvent event) {
 
-if (currentBet>= state.getRoom().getMinBet()){
-  panel.enableButton(GameButtonType.DEAL, true);
-  }
+		if (GameState.getTurn() != TurnState.AWAITING_BET &&
+				GameState.getTurn() != TurnState.AWAITING_DEAL
+				|| GameState.getBetAmount() == GameState.getRoom().getMaxBet()) {
+			return;
+		}
+		GameState.setTurn(TurnState.AWAITING_DEAL);
+		int currentBet = GameState.getBetAmount() + betAmount;
+		GameState.setBetAmount(currentBet);
 
-if (currentBet> state.getRoom().getMaxBet()){
-    currentBet=state.getRoom().getMaxBet();
-    panel.chipsEnabled(false);
-}
+		SoundManager.play(SoundName.CHIP_BET);
 
-//Fire the event so the rest of the UI knows that the action occurred
-event.setActionType(ActionType.BET);
-Events.eventBus.fireEvent(event);
-  }
+		if (currentBet >= GameState.getRoom().getMinBet()) {
+			panel.enableButton(GameButtonType.DEAL, true);
+		}
+
+		if (currentBet >= GameState.getRoom().getMaxBet()) {
+			currentBet = GameState.getRoom().getMaxBet();
+			GameState.setBetAmount(currentBet);
+			panel.chipsEnabled(false);
+		}
+		panel.bet(currentBet);
+
+		// Fire the event so the rest of the UI knows that the action occurred
+		event.setActionType(ActionType.BET);
+		Events.eventBus.fireEvent(event);
+	}
 }
