0bdd3a9423bd58e4db01f6bb3c0c7512a3ce7d3b
StephDoll <Stephaniengufor@gmail.com>
2017-02-22 20:43:30 -0600

    updated SurrenderAction

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/SurrenderAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/SurrenderAction.java
@@ -1,7 +1,9 @@
 package com.blackjack.client.action;
 
+import com.blackjack.client.entities.GameState;
 import com.blackjack.client.event.GameEvent;
 import com.blackjack.client.ui.BlackJackGamePanel;
+import com.blackjack.client.action.HandEndAction;
 
 public class SurrenderAction extends GameAction {
 
@@ -11,7 +13,13 @@ public class SurrenderAction extends GameAction {
 
 	@Override
 	public void processAction(GameEvent event) {
-		// TODO Auto-generated method stub
+		panel.displayInstruction("Surrendered");
+		event.setActionType(ActionType.SURRENDER);
+		event.getGameState().setSurrender(true);
+		GameState.getUser().setBankAmount(GameState.getUser().getBankAmount()+GameState.getBetAmount()/2);
+		//TODO Update bank amount display
+		HandEndAction handEndAction = new HandEndAction(panel);
+		handEndAction.processAction(event);
 
 	}
 
