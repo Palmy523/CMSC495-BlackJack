c2fbe3d94fefa1b839209e85610f8c89216f8725
Lea <lere.8811@gmail.com>
2017-02-25 23:19:00 -0500

    added split action flags

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/SplitAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/SplitAction.java
@@ -1,5 +1,6 @@
 package com.blackjack.client.action;
 
+import com.blackjack.client.event.Events;
 import com.blackjack.client.event.GameEvent;
 import com.blackjack.client.ui.BlackJackGamePanel;
 
@@ -12,7 +13,15 @@ public class SplitAction extends GameAction {
 	@Override
 	public void processAction(GameEvent event) {
 		// TODO Auto-generated method stub
+		
+		panel.getSplitButton().setEnabled(false);
+		
+		//Splits players hand into two separate hands
 		panel.splitPlayerHand();
+		event.getGameState().setSplit(true);
+		
+		event.setActionType(ActionType.SPLIT);
+		Events.eventBus.fireEvent(event);
 	}
 
 }
