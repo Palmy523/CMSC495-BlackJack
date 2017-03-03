01593702fc5a01f47b32837173250a29987acb08
seamusvibe <seamusvibe@gmail.com>
2017-03-02 19:12:35 -0500

    fixed surrender-push issue

--- Code/CMSC495-BlackJack/src/com/blackjack/client/action/HandEndAction.java
+++ Code/CMSC495-BlackJack/src/com/blackjack/client/action/HandEndAction.java
@@ -30,23 +30,29 @@ public class HandEndAction extends GameAction {
 	@SuppressWarnings("static-access")
 	@Override
 	public void processAction(GameEvent event) {
+		float easyPlayMultiplier = 1f;
+		if(GameState.isEasyPlay())
+			easyPlayMultiplier = .5f;
 		int playerHandVal = GameState.getPlayerHand().getHandValue();
 		int dealerHandVal = GameState.getDealerHand().getHandValue();
 		
 		if (event.getActionType() == ActionType.BLACKJACK) {
 			panel.displayInstruction("Blackjack!" + message);
 			SoundManager.play(SoundName.WIN);
-			float award = GameState.getBetAmount() + (GameState.getBetAmount() * (3/2));
+			float award = GameState.getBetAmount() + (GameState.getBetAmount() * (3/2) * easyPlayMultiplier);
 			UserController.updateChipCount(award);
 		} else if (event.getActionType() == ActionType.DEALER_BLACKJACK) {
 			panel.displayInstruction("Dealer has Blackjack!" + message);
 			
 			if (event.getGameState().isInsurance()) {
-				UserController.updateChipCount(GameState.getBetAmount()/* *2 */
+				UserController.updateChipCount(GameState.getBetAmount()
 						+ GameState.getInsuranceBetAmt());
 				panel.displayInsuranceBet(false);
 			}
 			panel.displayInsuranceBet(false);
+		} else if (event.getActionType() == ActionType.SURRENDER) {
+			panel.displayInstruction("You surrendered. You get half your bet back." + message);
+			UserController.updateChipCount((float)GameState.getBetAmount() / 2);
 		} else if (event.getActionType() == ActionType.PUSH || 
 				playerHandVal == dealerHandVal) {
 			panel.displayInstruction("Push!" + message);
@@ -56,16 +62,13 @@ public class HandEndAction extends GameAction {
 		} else if (event.getActionType() == ActionType.DEALER_BUST) {
 			SoundManager.play(SoundName.WIN);
 			panel.displayInstruction("You win!" + message);
-			float award = GameState.getBetAmount() * 2;
+			float award = GameState.getBetAmount() + GameState.getBetAmount() * easyPlayMultiplier;
 			UserController.updateChipCount(award);
-		} else if (event.getActionType() == ActionType.SURRENDER) {
-			panel.displayInstruction("You surrendered. You get half your bet back." + message);
-			UserController.updateChipCount((float)GameState.getBetAmount() / 2);
 		} else if (dealerHandVal > playerHandVal) {
 			panel.displayInstruction("Dealer wins!" + message);
 		} else if (dealerHandVal < playerHandVal) {
 			panel.displayInstruction("You win!" + message);
-			UserController.updateChipCount(GameState.getBetAmount() * 2);
+			UserController.updateChipCount(GameState.getBetAmount() + GameState.getBetAmount() * easyPlayMultiplier);
 			SoundManager.play(SoundName.WIN);
 		}
 		
